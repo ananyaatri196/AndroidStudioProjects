403fff026fa24f8da13a107d8fc2e652a8076673
ananyaatri196 <ananyaatri196@gmail.com>
2024-01-27 19:34:48 +0530

    implicit intent

---
+++ ImplicitIntent/app/src/main/java/com/example/implicitintent/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.implicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Button button;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.instagram.com/the_book_times/"));
+                startActivity(intent);
+            }
+        });
+    }
+}
