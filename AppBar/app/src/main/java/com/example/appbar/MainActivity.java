18b9bbb635d602141d081ee9372253c8b3ff496a
ananyaatri196 <ananyaatri196@gmail.com>
2024-03-08 21:11:38 +0530

    app bar

---
+++ AppBar/app/src/main/java/com/example/appbar/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.appbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setNavigationIcon(R.drawable.ic_launcher_foreground);
+        setSupportActionBar(myToolbar);
+    }
+}
